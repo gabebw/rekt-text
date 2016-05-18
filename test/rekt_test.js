0614deef9a2eb84a1ce1e31280023703a802ce2d
Gabe Berke-Williams <gabebw@gabebw.com>
2016-05-17 17:54:51 -0700

    Change the text color too

--- test/rekt_test.js
+++ test/rekt_test.js
@@ -1,16 +1,23 @@
-describe("Rekt", function() {
+describe("Rekt", function(){
   var rekt;
 
-  beforeEach(function() {
+  beforeEach(function(){
     rekt = new Rekt("hello");
   });
 
-  it("changes the text size", function (){
+  it("returns 5 styled elements as HTML", function(){
+    var result = rekt.wreck();
+
+    expect(result).to.have.length(5);
+    expect(result[0]).to.match(/^<span/);
+  });
+
+  it("changes the text size", function(){
     var result = rekt.wreck();
 
     expect(result).to.have.length(5);
 
-    var fontSizes = _.map(result, function(item) {
+    var fontSizes = _.map(result, function(item){
       return parseFloat($(item).css("font-size").replace("em", ""));
     });
 
@@ -21,5 +28,15 @@ describe("Rekt", function() {
     expect(_.find(fontSizes, function(fontSize){
       return fontSize < 1;
     })).to.be.ok();
+  });
+
+  it("changes the text color", function(){
+    var result = rekt.wreck();
+
+    var colors = _.map(result, function(item){
+      return $(item).css("color");
+    });
+
+    expect(colors).to.contain("red");
   });
 });
